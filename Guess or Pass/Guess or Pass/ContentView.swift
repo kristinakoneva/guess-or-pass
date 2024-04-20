db08183080e73a448380d0fce2acd3ad2b346372
Kristina Koneva <kristina.koneva@infinum.com>
2024-04-20 13:01:42 +0200

    Create a new project

---
+++ Guess or Pass/Guess or Pass/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Guess or Pass
+//
+//  Created by Kristina Koneva on 20.4.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Guess or Pass/Guess or Pass/Guess_or_PassApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_or_PassApp.swift
+//  Guess or Pass
+//
+//  Created by Kristina Koneva on 20.4.24.
+//
+
+import SwiftUI
+
+@main
+struct Guess_or_PassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
